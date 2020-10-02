385a6bf403553bda032014ee05c9cdbb73d464d4
settawut-niy <settawut.niy@gmail.com>
2020-10-02 11:12:29 +0700

    Created fundamental system and ux of snake game
    - Snake Class
          - Created movement behavior
          - Created eat and grow behavior
          - Created lose condition
          - Created input manager
          - Contained related variable e.g. tailPrefab that use to make snake grow
    
    - Spawn Food
          - Created random spawn food (within range) behavior
          - Contained related variable e.g. foodPrefab
    
    - Other
         - Created playable scene
         - Imported simple 2D asset

---
+++ Assets/_Scripts/Snake.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Snake : MonoBehaviour
+{
+    // Current snake direction
+    Vector2 currentDir = Vector2.right;
+
+    // About tail
+    List<Transform> tail = new List<Transform>();
+    [SerializeField] GameObject tailPrefab;
+
+    // Eat behavior
+    bool ate = false;
+
+
+    void Start()
+    {
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    void Update()
+    {
+        InputManager();
+    }
+
+    void InputManager()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (currentDir != Vector2.down)
+            {
+                currentDir = Vector2.up;
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (currentDir != Vector2.up)
+            {
+                currentDir = Vector2.down;
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (currentDir != Vector2.left)
+            {
+                currentDir = Vector2.right;
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (currentDir != Vector2.right)
+            {
+                currentDir = Vector2.left;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.StartsWith("Food"))
+        {
+            ate = true;
+
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            SceneManager.LoadScene("Snake Scene");
+        }
+
+    }
+
+    void Move()
+    {
+        Vector2 currentPosition = transform.position;
+
+        transform.Translate(currentDir);
+
+        if (ate)
+        {
+            GameObject body = (GameObject)Instantiate(tailPrefab, currentPosition, Quaternion.identity);
+
+            tail.Insert(0,body.transform);
+
+            ate = false;
+        }
+        else if (tail.Count > 0)
+        {
+            tail.Last().position = currentPosition;
+
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count-1);
+        }
+    }
+}
---
+++ Assets/_Scripts/SpawnFood.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    // Food
+    [SerializeField] GameObject foodPrefab;
+
+    // Borders
+    [SerializeField] Transform borderTop;
+    [SerializeField] Transform borderBottom;
+    [SerializeField] Transform borderLeft;
+    [SerializeField] Transform borderRight;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn",3,4);
+    }
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(borderLeft.position.x+1, borderRight.position.x-1);
+        int y = (int)Random.Range(borderTop.position.y-1, borderBottom.position.y+1);
+
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+
+}
